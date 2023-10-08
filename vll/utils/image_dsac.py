266fab0ba8559b908e31390aa5d1adf9769efe0a
AnushaChattoHeidelberg <anusha.chattopadhyay@stud.uni-heidelberg.de>
2023-10-08 03:25:30 +0200

    value calc function writing

--- vll/utils/image_dsac.py
+++ vll/utils/image_dsac.py
@@ -176,8 +176,41 @@ class VanishingPointDSAC:
 
 	
 	def _valuecalc__(self,img,edges,lines,distances,accumulation_matrix):
+		height, width = img.shape
+		#create a distance matrix
+		distance_matrix = np.empty((height, width))
+		for i in range(width):
+			for j in range(height):
+				if accumulation_matrix[j, i] > 0:
+					distance_between_segment_and_vp = []
+					line2 = [(), ()]
+					#so I don't have to write another function I decide that the endpoints of line2 are the same
+					# Assign coordinates to index 0 (i, j)
+					line2[0] = (j, i)
+					# Assign coordinates to index 1 (i, j)
+					line2[1] = (j, i)
+					for k in range(len(lines)):
+						distance=self.calculate_distance_between_line_segments(lines[k], line2)
+						distance_between_segment_and_vp.append(distance)
+					#storing the distance array in the distance matrix for each line in the cell for a potential vanishing point
+					distance_matrix[j,i]=distance_between_segment_and_vp
+				else: distance_matrix[j,i] = []
+		#create a vote matrix
+		vote_matrix= np.empty((height, width))
+		for i in range(width):
+			for j in range(height):
+				if distance_matrix[j, i] != []:
+					votes = 0
+					for k in range(len(lines)):
+						#picking out the distance between a line and a point in the accumulation matrix
+						distance = distance_matrix[j,i][k]
+						#vote function to write vote function here
+						vote = 1
+						
+						votes += vote
+					distance_matrix[j,i] = votes
+					
 		
-		pass
 	
 	def _search_(self,img,edges):
 		pass
